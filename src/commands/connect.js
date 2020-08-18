5c0854a26bfef883219141746702d36747272675
Steven <nartnevets93@gmail.com>
2020-08-18 15:54:44 -0700

    got ffmpeg to do its thing (functioning, not actually passed through bumblebee though)

--- src/commands/connect.js
+++ src/commands/connect.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 // const {src, dst} = require('../../config/audio_format.json');
 const Bumblebee = require('bumblebee-hotword-node');
 const ffmpeg = require('fluent-ffmpeg');
+// const processSpawn = require('child_process')
 
 module.exports = createCommand("connect",
     "connect to server and set up streams.",
@@ -10,36 +11,31 @@ module.exports = createCommand("connect",
     {
         if (message.member.voice.channel)
         {
-            const connection = await message.member.voice.channel.join()
-                .then(message.channel.send(`Connected to ${message.member.voice.channel}.`));
+            const member = message.member;
+            const connection = await member.voice.channel.join()
+                .then(message.channel.send(`Connected to ${member.voice.channel}.`));
             const dispatcher = fixVoiceReceive(connection);
             const userStreams = [];
 
+            const audioStream = connection.receiver.createStream(member.user, {mode: 'pcm', end: 'manual'});
+
             // Make voice streams for voice commands
-            message.guild.me.voice.channel.members.forEach(member =>
-            {
-                if (member.user.id !== message.client.user.id)
-                {
-                    const audioStream = connection.receiver.createStream(member.user,
-                        {mode: 'pcm', end: 'manual'});
+            const command = new ffmpeg().input(audioStream)
+                .inputOptions(['-f s16le', '-ac 2', '-ar 48000'])
+                .outputOptions(['-ac 1', '-ar 16000']).pipe();
 
-                    const command = new ffmpeg(audioStream).inputFormat('s16le').audioChannels(1).audioFrequency(16000);
+            // command.on('error', err => console.log(err));
+            // command.on('end', (stdout, stderr) => console.log('we finished :0'));
 
-                    command.on('error', err => console.log(err));
-                    command.on('end', (stdout, stderr) => console.log('we finished :0'));
+            // Voice Recognition Package
+            const bumblebee = new Bumblebee();
+            bumblebee.addHotword('bumblebee');
+            bumblebee.on('hotword', hotword => console.log('hot word detected: ' + hotword));
+            bumblebee.start({stream: command});
 
-                    // Voice Recognition Package
-                    // const bumblebee = new Bumblebee();
-                    // bumblebee.addHotword('bumblebee');
-                    // bumblebee.on('hotword', function (hotword) {
-                    //     console.log('Hotword Detected:', hotword);
-                    // });
-                    // bumblebee.start({stream: transcode});
+            console.log(`created audio stream for ${member.user}`);
 
-                    userStreams.push(audioStream);
-                    console.log(`created audio stream for ${member.user}`);
-                }
-            });
+            userStreams.push(audioStream);
 
             // Store the connection to the server, the dispatcher to the server, and voice streams of each user in the voice channel.
             message.client.voiceConnections.set(message.guild.id, createVoiceConnectionData(connection, dispatcher, userStreams));
--- temp_voices/ffmpegTest.js
+++ temp_voices/ffmpegTest.js
@@ -1,3 +1,3 @@
 const ffmpeg = require('fluent-ffmpeg');
 
-const command = new ffmpeg('pre_Steven_noedits.raw').inputFormat('s16le').audioChannels(1).audioFrequency(16000).saveToFile('ffmpeg_output.wav');
+const command = new ffmpeg().input('pre_Steven_noedits.raw').inputFormat('s16le').audioChannels(1).audioFrequency(16000).saveToFile('ffmpeg_output.wav');
